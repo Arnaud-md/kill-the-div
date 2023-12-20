aa277f8abc14aedc2552fc9d2341475397faac83
Arnaud Laforgue <arnaud.laforgue@gmail.com>
2023-12-20 16:45:36 +0100

    avant fetch geolocalisation

--- src/Game.tsx
+++ src/Game.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const son=new Audio("/short-success-sound-glockenspiel-treasure-video-game-6346.mp3");
 
+
 const Game = () => {
     const [left,setLeft]=useState(Math.random()*100);
     const [top,setTop]=useState(Math.random()*100);
@@ -45,6 +46,7 @@ const Game = () => {
             // navigate('/end');
         }
     },[nbClick]);
+    
     const handleRegame = useCallback(async()=> {
         // left = Math.random()*100;
         // top = Math.random()*100;
@@ -53,42 +55,58 @@ const Game = () => {
         setTop(Math.random()*100);
         setNbClick(0);
     },[]);
+    
     const handleClickPause = useCallback(()=>{
         setDoPause(!doPause);
         // setDate(Date.now());
-        if(!doPause) {
+        if(doPause) {
             setDebut(Date.now());
         }
-        if(doPause) {
+        if(!doPause) {
+            // setDebut(Date.now());
             setAcc(acc+Date.now()-debut);
         }
         //setStartTime(startTime+Date.now()-date);
 
-    },[doPause,date,startTime])
-    const getPosition = (()=>{
-        navigator.geolocation.getCurrentPosition((position)=> {
+    },[doPause,acc])
+
+    const handleClickLocalisation = useCallback(async()=>{
+        navigator.geolocation.getCurrentPosition(async(position)=> {
+            
             setLatitude(position.coords.latitude);
             setLongitude(position.coords.longitude);
+            let lat = position.coords.latitude.toString();
+            let long = position.coords.longitude.toString();
+
+            const response = await fetch('https://api.bigdatacloud.net/data/reverse-geocode-client?latitude='+lat+'&longitude='+long+'&localityLanguage=en');
+            console.log('response',response);
+            
         })
-    })
-    getPosition();
-    console.log("doPause :", doPause);
-    console.log("secondes : ",(Date.now() - startTime)/1000);
-    console.log("latitude : ",latitude);
-    console.log("longitude : ",longitude);
+
+    },[])
+
+
+    // console.log("doPause :", doPause);
+    // console.log("secondes : ",(acc+Date.now()-debut)/1000);
+    // console.log("latitude : ",latitude);
+    // console.log("longitude : ",longitude);
+
     return (
         <div className="game">
             <div className="compteur">
                 <p className="score">Nombre de div cliquées : {nbClick} / 10</p>
                 {/* <p className="score">Time : {!doPause ? (Date.now() - startTime)/1000 : (date-startTime)/1000 } s</p> */}
-                <p className="score">Time : {doPause ? (acc+Date.now()-debut)/1000 : (acc)/1000 } s</p>
+                {/* <p className="score">Time : {doPause ? (acc+Date.now()-debut)/1000 : acc/1000 } s</p> */}
+                <p className="score">Time : {doPause ? acc/1000  : (acc+Date.now()-debut)/1000 } s</p>
+                <button onClick={handleClickLocalisation}>Localisation</button>
             </div>
-            {nbClick!==10 ?
+            <div onClick={handleClick} className="round" style={{top: top + "%", left:left + "%"}}></div>
+            {/* {nbClick!==10 ?
             <div onClick={handleClick} className="round" style={{top: top + "%", left:left + "%"}}></div> :
             <div className="end">
                 <p>Fin de partie</p>
                 <button onClick={handleRegame}>Rejouer</button>    
-            </div>}
+            </div>} */}
             <div>
                 <button className="pause" onClick={handleClickPause}>Pause</button>
             </div>
